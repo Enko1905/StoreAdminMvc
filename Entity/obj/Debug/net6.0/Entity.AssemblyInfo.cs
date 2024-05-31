c12382051a504ec6cf5d8c25113e8a5b70736cb1
Süleyman Enes KOÇ <enkoc1905@hotmail.com>
2024-05-31 21:02:31 +0300

    test1

--- Entity/obj/Debug/net6.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net6.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80378a889c699319c689ade87687a7f022ca21b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
